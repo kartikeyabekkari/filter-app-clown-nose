11bb7a5ddb76298879cd1d18a28f034bfb0ae1e9
kartikeyabekkari <83190693+kartikeyabekkari@users.noreply.github.com>
2022-08-28 19:19:55 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,40 @@
+noseX = 0
+noseY = 0
+
+function preload() {
+    clown_nose = loadImage('https://i.postimg.cc/rm7L9KMZ/clown-nose-clipart-2.png')
+}
+
+function setup() {
+    canvas = createCanvas(300,300);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(300, 300);
+    video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function draw() {
+    image(video,0,0,300,300);
+    image(clown_nose, noseX, noseY, 30, 30);
+
+}
+
+function take_snapshot() {
+    save('myClownImage.png');
+}
+
+function modelLoaded() {
+    console.log('PoseNet Is Initialized');
+}
+
+function gotPoses(results) {
+    if(results.length > 0) {
+        noseX = results[0].pose.nose.x-10;
+        noseY = results[0].pose.nose.y-10;
+        console.log("nose x = " + results[0].pose.nose.x);
+        console.log("nose y = " + results[0].pose.nose.y);
+    }
+}
